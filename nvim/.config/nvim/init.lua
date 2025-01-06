9096a3a7d68ebe169d329f51a0b4c1748a740070
Donatas Tubutis <dotubutis@gmail.com>
2025-01-06 10:03:49 -0500

    add gruvox nvim theme

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1007,7 +1007,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-moon'
+      -- vim.cmd.colorscheme 'tokyonight-moon'
+      vim.cmd.colorscheme 'gruvbox'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -1306,6 +1307,9 @@ require('lazy').setup({
       },
     },
   },
+  { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true },
+  -- END OF PLUGINS
+
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
