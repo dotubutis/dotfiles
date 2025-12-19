b385c8c30887029e543d9c82f098caac8802211d
Donatas Tubutis <dotubutis@gmail.com>
2025-12-19 10:26:03 -0500

    nvim solargraph config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -743,7 +743,14 @@ require('lazy').setup({
         -- clangd = {},
         gopls = {},
         -- buf = {},
-        solargraph = {},
+        solargraph = {
+          settings = {
+            solargraph = {
+              diagnostics = true,
+            },
+          },
+          init_options = { formatting = true },
+        },
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
